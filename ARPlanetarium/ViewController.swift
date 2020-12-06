9b5882c13372b54ee1856c48bde5fd242f23bed4
Oscar Castillo <goldoac@live.com>
2020-12-06 16:05:47 -0600

    initial project

---
+++ ARPlanetarium/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ARPlanetarium
+//
+//  Created by Oscar Castillo on 12/6/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
