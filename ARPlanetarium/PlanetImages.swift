69bea71d1f889a5565aacdaea783f6e42f475189
Oscar Castillo <goldoac@live.com>
2020-12-06 23:20:59 -0600

    adds the sun node and the enum to map all the images to prevent using strings.

---
+++ ARPlanetarium/PlanetImages.swift
@@ -0,0 +1,30 @@
+//
+//  PlanetImages.swift
+//  ARPlanetarium
+//
+//  Created by Oscar Castillo on 12/6/20.
+//
+
+import UIKit
+
+enum PlanetImages: String {
+    case sunDiffuse
+    case mercuryDiffuse
+    case venusDiffuse
+    case venusSpecular
+    case earthDiffuse
+    case earthSpecular
+    case earthNormal
+    case earthClouds
+    case marsDiffuse
+    case jupiterDiffuse
+    case saturnDiffuse
+    case saturnRingDiffuse
+    case uranusDiffuse
+    case neptuneDiffuse
+    
+    func makeImage() -> UIImage {
+        UIImage(named: self.rawValue) ?? UIImage()
+    }
+}
+
--- ARPlanetarium/ViewController.swift
+++ ARPlanetarium/ViewController.swift
@@ -20,5 +20,36 @@ class ViewController: UIViewController {
         sceneView.debugOptions = [.showFeaturePoints, .showWorldOrigin]
         sceneView.session.run(configuration)
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        createSun()
+    }
+    
+    func createSun() {
+        let sun = createPlanetNode(geometry: SCNSphere(radius: 0.3),
+                                   position: SCNVector3(0,0,-1),
+                                   diffuse: .sunDiffuse)
+        sceneView.scene.rootNode.addChildNode(sun)
+        sun.geometry?.firstMaterial?.diffuse.contents = PlanetImages.sunDiffuse.makeImage()
+    }
+    
+    func createPlanetNode(geometry: SCNGeometry,
+                          position: SCNVector3,
+                          diffuse: PlanetImages,
+                          specular: PlanetImages? = nil,
+                          normal: PlanetImages? = nil,
+                          emission: PlanetImages? = nil) -> SCNNode {
+        let planetNode = SCNNode(geometry: geometry)
+        planetNode.geometry?.firstMaterial?.diffuse.contents = diffuse.makeImage()
+        planetNode.geometry?.firstMaterial?.specular.contents = specular?.makeImage()
+        planetNode.geometry?.firstMaterial?.normal.contents = normal?.makeImage()
+        planetNode.geometry?.firstMaterial?.emission.contents = emission?.makeImage()
+        planetNode.position = position
+        
+        return planetNode
+    }
 }
+
 
